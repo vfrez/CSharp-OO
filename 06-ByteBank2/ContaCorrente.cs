4df460336132473c74a8287f98b8c31534a31955
Vitor <vih.frez@gmail.com>
2019-01-17 17:58:00 -0200

    Dia 4

---
+++ 06-ByteBank2/ContaCorrente.cs
@@ -0,0 +1,65 @@
+﻿//using _05_ByteBank; //Para não precisar indicar o nameSpace na frente da Classe, pode-se adicionar o mesmo no using, no inicio no arquivo.
+
+namespace _06_ByteBank2 //Desta maneira, todas as classes que estão neste nameSpace são encontradas pelo C#
+{
+    public class ContaCorrente
+    {
+        //Quando não declararmos valores para os objetos instanciados, será atribuido um valor padrão
+        //Caso não indicarmos aqui na criação da classe o valor, o mesmo sera: vazio para string, e 0 para numeros
+        //Caso queiramos trocar este valor padrão, só atribuir na criação dos atributos o valor que queira
+
+        //public string titular;
+        //public _05_ByteBank.Cliente titular; //Para usar classes de outros nameSpaces, é necessário informar a classe nameSpace.Classe, desta maneira ele encontra a Classe
+        //public Cliente titular;//utilizando using
+        public Cliente titular;// Referencia a classe pessoa e todos os seus atributos e talvez métodos;
+        public int agencia;
+        public int numero;
+        private double saldo = 100; //Novo valor padrão
+
+     
+
+        public void SetSaldo(double saldo)
+        {
+            if(saldo < 0)
+            {
+                return;
+            }
+            this.saldo -= saldo;
+        }
+
+        public double GetSaldo ()
+        {
+            return saldo;
+        }
+        public bool Sacar(double valor)
+        {
+            if (this.saldo <= valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+        //Chama outra referencia, com todos os metodos e atributos.
+        public bool Transferir(double valor, ContaCorrente contaTransferir)
+        {
+            if (this.saldo < valor)
+            {
+                return true;//Return true para a função no momento de sua execução
+            }
+
+            this.saldo -= valor;
+            contaTransferir.saldo += valor;
+            return true;
+        }
+
+    }
+}
---
+++ 06-ByteBank2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente contaPessoa = new ContaCorrente();
+
+            contaPessoa.SetSaldo(-10);//Esta maneira é utilizando com o metodo SetSaldo, criado separadamente 
+
+            Console.WriteLine(contaPessoa.GetSaldo());// Esta maneira é utilizando com o metodo GetSaldo, criado separadamente
+
+           
+
+            Console.ReadLine();
+        }
+    }
+}
