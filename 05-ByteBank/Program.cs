a2664dfa64fe0fc9bd2cacbb3116e38ac3f6b334
Vitor <vih.frez@gmail.com>
2019-01-17 16:41:46 -0200

    Separação da aula 5

--- 05-ByteBank/Program.cs
+++ 05-ByteBank/Program.cs
@@ -18,32 +18,26 @@ namespace _05_ByteBank
 
             ContaCorrente contaPessoa1 = new ContaCorrente();
             //contaPessoa1.titular = pessoa1; //Apontando a referencia do objeto criado pessoa1, com todos os dados presentes nesta referencia.
-            //contaPessoa1.titular = new Cliente();//Pode-se criar a referencia desta maneira.
-            //contaPessoa1.titular.nome = "Pessoa2";//Se não for instaciado, mesmo que da maneira realizado acima, não funciona
-            //contaPessoa1.titular.cpf = "123.456.789.-12";
-            //contaPessoa1.titular.profissao = "Sô trabalhadô";
+            contaPessoa1.titular = new Cliente();//Pode-se criar a referencia desta maneira.
+            contaPessoa1.titular.nome = "Pessoa2";//Se não for instaciado, mesmo que da maneira realizado acima, não funciona
+            contaPessoa1.titular.cpf = "123.456.789.-12";
+            contaPessoa1.titular.profissao = "Sô trabalhadô";
             contaPessoa1.numero = 123;
             contaPessoa1.agencia = 123456;
 
             //Console.WriteLine("Nome Pessoa: " + pessoa1.nome);
-            //Console.WriteLine("Titular conta: " + contaPessoa1.titular.nome); // pode-se acessar os dados do objeto pessoa1 que foi refenciado no contaPessoa1, adicionando todo o caminho, no caso, nome da pessoa.
+            Console.WriteLine("Titular conta: " + contaPessoa1.titular.nome); // pode-se acessar os dados do objeto pessoa1 que foi refenciado no contaPessoa1, adicionando todo o caminho, no caso, nome da pessoa.
 
             //se alterar o nome da pessoa a partir da classe contaPessoa 1, será alterado tambem para o objeto pessoa1, pois  a referencia na memoria do computador é a mesma para os dois.
             //contaPessoa1.titular.nome = "Pessoa1 Alterada";
             //Console.WriteLine("Nome Pessoa depois de alterar: " + pessoa1.nome);
-            //Console.WriteLine("Titular conta depois de alterar: " + contaPessoa1.titular.nome);
-            //Console.WriteLine(contaPessoa1.titular.nome);// se não for instanciado de alguma maneira não funciona para consulta tambem
-            //Console.WriteLine(contaPessoa1.titular.cpf);
-            //Console.WriteLine(contaPessoa1.titular.profissao);
+            Console.WriteLine("Titular conta depois de alterar: " + contaPessoa1.titular.nome);
+            Console.WriteLine(contaPessoa1.titular.nome);// se não for instanciado de alguma maneira não funciona para consulta tambem
+            Console.WriteLine(contaPessoa1.titular.cpf);
+            Console.WriteLine(contaPessoa1.titular.profissao);
 
-            //pode-se tentar apresentar o campo titular em tela, mesmo que não tenha dados, porem não se pode tentar mostrar o nome do titular, pois a referencia de cliente não existe em memoria;
-            Console.WriteLine("Titular sem referencia, contaPessoa1.titular" + contaPessoa1.titular);
-
-            if(contaPessoa1.titular == null)
-            {
-                Console.WriteLine("A referencia contaPessoa1.titular é NULL");
-            }
-
+           
+          
             Console.ReadLine();
         }
     }
---
+++ 05-ByteBank2/ContaCorrente.cs
@@ -0,0 +1,50 @@
+﻿//using _05_ByteBank; //Para não precisar indicar o nameSpace na frente da Classe, pode-se adicionar o mesmo no using, no inicio no arquivo.
+
+namespace _05_ByteBank2 //Desta maneira, todas as classes que estão neste nameSpace são encontradas pelo C#
+{
+    public class ContaCorrente
+    {
+        //Quando não declararmos valores para os objetos instanciados, será atribuido um valor padrão
+        //Caso não indicarmos aqui na criação da classe o valor, o mesmo sera: vazio para string, e 0 para numeros
+        //Caso queiramos trocar este valor padrão, só atribuir na criação dos atributos o valor que queira
+
+        //public string titular;
+        //public _05_ByteBank.Cliente titular; //Para usar classes de outros nameSpaces, é necessário informar a classe nameSpace.Classe, desta maneira ele encontra a Classe
+        //public Cliente titular;//utilizando using
+        public Cliente titular;// Referencia a classe pessoa e todos os seus atributos e talvez métodos;
+        public int agencia;
+        public int numero;
+        public double saldo = 100; //Novo valor padrão
+
+        public bool Sacar(double valor)
+        {
+            if (this.saldo <= valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+        //Chama outra referencia, com todos os metodos e atributos.
+        public bool Transferir(double valor, ContaCorrente contaTransferir)
+        {
+            if (this.saldo < valor)
+            {
+                return true;//Return true para a função no momento de sua execução
+            }
+
+            this.saldo -= valor;
+            contaTransferir.saldo += valor;
+            return true;
+        }
+
+    }
+}
